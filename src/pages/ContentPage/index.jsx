d08a4fc01fe381d0cabce98a5a297fe104d85bf7
Sneha Trivedi <snehatrivedi1009@gmail.com>
2023-03-10 23:07:05 +0530

    Chore: create some modifications in the content page

--- src/pages/ContentPage/index.jsx
+++ src/pages/ContentPage/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-// import Header from "../../components/Header/index.jsx";
 import "./ContentPage.css";
 import Body from "../../components/Body/index.jsx";
 function Content() {
   return (
     <>
-    {/* <Header/> */}
-    <Body/>
-
-       
+      <Body />
     </>
   );
 }
